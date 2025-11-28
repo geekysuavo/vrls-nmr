bca4aa76486dca22fac0b418284c092c0325f432
Bradley Worley <geekysuavo@gmail.com>
2025-11-28 00:55:35 +0000

    Final figure

--- notebooks/ans-benchmark.py
+++ notebooks/ans-benchmark.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.18.0"
+__generated_with = "0.18.1"
 app = marimo.App(width="medium")
 
 
@@ -15,6 +15,8 @@ def _():
     from functools import partial, reduce
     from itertools import product
     import operator
+    from pathlib import Path
+    import time
 
     import matplotlib.pyplot as plt
     import pandas as pd
@@ -24,7 +26,20 @@ def _():
 
     import vrlsnmr.algorithms as algo
     import vrlsnmr.simulators as sim
-    return algo, operator, partial, pd, product, reduce, sim, sns, torch
+    return (
+        Path,
+        algo,
+        operator,
+        partial,
+        pd,
+        plt,
+        product,
+        reduce,
+        sim,
+        sns,
+        time,
+        torch,
+    )
 
 
 @app.function
@@ -37,29 +52,26 @@ def _(operator, reduce):
     n = 2048
     niter = 100
     decay_rate = 0.001
-    num_replicates = 10
 
-    replicate_values = tuple(range(num_replicates))
-    sigma_values = (0.05, 0.1, 0.2)
-    ncomp_values = (4, 8)
-    resolution_values = (1, 2, 10)
-    ampl_dof_values = (1, 10, 50)
-    sparsity_values = (0.05, 0.1, 0.2)
+    sigma_values = (0.01, 0.05, 0.1)
+    ncomp_values = (4, 8, 12)
+    ampl_dof_values = (1, 10, 50,)
+    sparsity_values = (  # sparsity-dependent num_replicas
+        (0.05,) * 10 +
+        (0.1,) * 5 +
+        (0.2,) * 2
+    )
 
     space_names = (
-        "replicate",
         "sigma",
         "num_components",
-        "resolution",
         "amplitude_dof",
         "min_sparsity",
     )
 
     space = (
-        replicate_values,
         sigma_values,
         ncomp_values,
-        resolution_values,
         ampl_dof_values,
         sparsity_values,
     )
@@ -83,6 +95,7 @@ def _(
     sim,
     space,
     space_names,
+    time,
     torch,
 ):
     data = []
@@ -91,13 +104,11 @@ def _(
         product(*space),
         total=reduce(operator.mul, map(len, space)),
     ):
-        (rep, sigma, num_components, resolution, amplitude_dof, min_sparsity) = point
-
-        spacing = resolution * decay_rate
+        (sigma, num_components, amplitude_dof, min_sparsity) = point
 
         ground_truth = sim.Signal.build(
             num_components=num_components,
-            frequencies=partial(sim.random_spaced, lower=-0.5, upper=0.5, space=spacing),
+            frequencies=partial(sim.random_spaced, lower=-0.5, upper=0.5, space=2 * decay_rate),
             decayrates=partial(sim.random_chisq, dof=20, mean=decay_rate),
             amplitudes=partial(sim.random_chisq, dof=amplitude_dof, mean=1.0),
             phases=partial(sim.random_normal, mean=0.0, stdev=1.0e-6),
@@ -112,7 +123,8 @@ def _(
         m_initial = next_power_of_two(int(m_final // 8))
         n_initial = m_initial * 4
 
-        (y, ids, mu, gamma_diag, yhat, sigma_diag) = algo.ans(
+        t0 = time.perf_counter()
+        (y, ids, mu, gamma_diag, _, _) = algo.ans(
             model=algo.vrls,
             measure=partial(ground_truth, noise=sigma),
             m_initial=m_initial,
@@ -124,42 +136,148 @@ def _(
             xi=tau,
             niter=niter,
         )
+        dt = time.perf_counter() - t0
 
         mu.squeeze_(dim=0)
+        gamma_diag.squeeze_(dim=0)
+    
         n_fd = mu.size(dim=0)
         t = torch.arange(n_fd, device=mu.device)
         y0 = ground_truth(t).select(dim=0, index=0)
         x0 = torch.fft.fft(y0, norm="ortho").roll(n_fd // 2)
+        x0.real -= x0.real.median()
+        mask = x0.real.gt(sigma)
 
-        mse = (mu - x0).abs().square().mean()
+        signal_mse = (mu - x0)[mask].abs().square().mean().item()
+        noise_mse = (mu - x0)[~mask].abs().square().mean().item()
+        total_mse = (mu - x0).abs().square().mean().item()
+
+        signal_var = gamma_diag[mask].mean().item()
+        noise_var = gamma_diag[~mask].mean().item()
+        total_var = gamma_diag.mean().item()
 
         entry = dict(zip(space_names, point))
         entry["m_initial"] = m_initial
         entry["m_final"] = m_final
         entry["n_initial"] = n_initial
         entry["n_final"] = n_final
-        entry["mse"] = mse.item()
+        entry["mse_signal_ans"] = signal_mse
+        entry["mse_noise_ans"] = noise_mse
+        entry["mse_total_ans"] = total_mse
+        entry["var_signal_ans"] = signal_var
+        entry["var_noise_ans"] = noise_var
+        entry["var_total_ans"] = total_var
+        entry["time_ans"] = dt / niter
+
+        ids_unif = (
+            torch.randperm(n_fd // 2, device=ids.device)
+            .narrow(dim=0, start=0, length=m_final)
+        )
+        y_unif = ground_truth(ids_unif, noise=sigma)
+        t0 = time.perf_counter()
+        (mu_unif, gamma_unif, _, _) = algo.vrls(
+            y_unif,
+            ids_unif,
+            tau=tau,
+            xi=tau,
+            n=n_fd,
+            niter=niter,
+        )
+        dt = time.perf_counter() - t0
+    
+        mu_unif = mu_unif.squeeze(dim=0).roll(n_fd // 2)
+        gamma_unif = gamma_unif.squeeze(dim=0).roll(n_fd // 2)
+    
+        signal_mse = (mu_unif - x0)[mask].abs().square().mean().item()
+        noise_mse = (mu_unif - x0)[~mask].abs().square().mean().item()
+        total_mse = (mu_unif - x0).abs().square().mean().item()
+
+        signal_var = gamma_unif[mask].mean().item()
+        noise_var = gamma_unif[~mask].mean().item()
+        total_var = gamma_unif.mean().item()
+
+        entry["mse_signal_unif"] = signal_mse
+        entry["mse_noise_unif"] = noise_mse
+        entry["mse_total_unif"] = total_mse
+        entry["var_signal_unif"] = signal_var
+        entry["var_noise_unif"] = noise_var
+        entry["var_total_unif"] = total_var
+        entry["time_unif"] = dt
         data.append(entry)
 
     df = pd.DataFrame(data)
     df
-    return df, ids
+    return df, mask, mu, mu_unif, x0
 
 
 @app.cell
-def _(ids):
-    ids
+def _(df):
+    df["var_noise_diff"] = (df.var_noise_unif - df.var_noise_ans) / df.var_noise_unif
+    df["mse_noise_diff"] = (df.mse_noise_unif - df.mse_noise_ans) / df.mse_noise_unif
     return
 
 
 @app.cell
-def _(df, sns):
-    sns.histplot(x="mse", hue="sigma", data=df)
+def _(df):
+    agg = df.groupby(["sigma", "min_sparsity"])[["mse_noise_ans", "mse_noise_unif", "mse_noise_diff"]].mean()
+    agg
     return
 
 
 @app.cell
-def _():
+def _(Path, df, plt, sns):
+    (fig, ax) = plt.subplots(figsize=(6, 4))
+
+    sns.barplot(
+        x="sigma",
+        y="mse_noise_diff",
+        hue="min_sparsity",
+        data=df,
+        ax=ax,
+        palette=[
+            (0.8,) * 3,
+            (0.6,) * 3,
+            (0.4,) * 3,
+        ]
+    )
+
+    ax.set_xlabel(r"Measurement noise ($\sigma$)")
+    ax.set_ylabel("Relative error reduction")
+    ax.grid(color=(0.9,) * 3)
+    ax.set_axisbelow(True)
+
+    legend = ax.get_legend()
+    legend.set_title("Undersampling")
+    legend.texts[0].set_text("5%")
+    legend.texts[1].set_text("10%")
+    legend.texts[2].set_text("20%")
+
+    plt.savefig(
+        Path.cwd() / "figure-3.pdf",
+        format="pdf",
+        dpi=600,
+        pad_inches=0,
+        bbox_inches="tight",
+    )
+
+    fig
+    return
+
+
+@app.cell
+def _(mask, mu, mu_unif, plt, x0):
+    (_, _ax) = plt.subplots()
+
+    _ax.plot(x0.real.cpu())
+    _ax.plot(mu.real.cpu())
+    _ax.plot(mu_unif.real.cpu())
+    _ax.plot(mask.cpu())
+    _ax.grid(alpha=0.2)
+
+    _ax.set_xlim((900, 1024))
+    #_ax.set_ylim((-1, 1))
+
+    _ax
     return
 
 
--- notebooks/ans-simulation.py
+++ notebooks/ans-simulation.py
@@ -1,12 +1,13 @@
 import marimo
 
-__generated_with = "0.17.8"
+__generated_with = "0.18.1"
 app = marimo.App(width="medium")
 
 
 @app.cell
 def _():
     from functools import partial
+    from pathlib import Path
 
     import matplotlib.pyplot as plt
     import torch
@@ -14,16 +15,18 @@ def _():
 
     import vrlsnmr.algorithms as algo
     import vrlsnmr.simulators as sim
-    return algo, partial, plt, sim, torch
+    return Path, algo, partial, plt, sim, torch
 
 
 @app.cell
-def _(partial, sim):
+def _(partial, sim, torch):
+    torch.random.manual_seed(1729)
+
     ground_truth = sim.Signal.build(
         num_components=8,
         frequencies=partial(sim.random_spaced, lower=-0.5, upper=0.5, space=0.002),
         decayrates=partial(sim.random_chisq, dof=20, mean=0.001),
-        amplitudes=partial(sim.random_chisq, dof=10, mean=1.0),
+        amplitudes=partial(sim.random_chisq, dof=50, mean=1.0),
         phases=partial(sim.random_normal, mean=0.0, stdev=1.0e-6),
     )
 
@@ -52,7 +55,41 @@ def _(algo, ground_truth, partial):
         xi=xi,
         niter=niter,
     )
-    return ids, mu, sigma, sigma_diag, y, yhat
+
+    y.squeeze_(dim=0)
+
+    mu.squeeze_(dim=0)
+    gamma_diag.squeeze_(dim=0)
+
+    yhat = yhat.squeeze(dim=0)
+    sigma_diag = sigma_diag.squeeze(dim=0)
+
+    n_fd = mu.size(dim=0)
+    n_td = yhat.size(dim=0)
+    return ids, mu, n_fd, n_td, niter, sigma, sigma_diag, tau, xi, y, yhat
+
+
+@app.cell
+def _(algo, ground_truth, ids, n_fd, n_td, niter, sigma, tau, torch, xi):
+    ids_unif = (
+        torch.randperm(n_td, device=ids.device)
+        .narrow(dim=0, start=0, length=ids.numel())
+    )
+
+    y_unif = ground_truth(ids_unif, noise=sigma).squeeze(dim=0)
+
+    (mu_unif, gamma_unif, _, _) = algo.vrls(
+        y_unif,
+        ids_unif,
+        tau=tau,
+        xi=xi,
+        n=n_fd,
+        niter=niter,
+    )
+
+    mu_unif = mu_unif.squeeze(dim=0).roll(n_fd // 2)
+    gamma_unif = gamma_unif.squeeze(dim=0).roll(n_fd // 2)
+    return (mu_unif,)
 
 
 @app.cell
@@ -62,35 +99,78 @@ def _(ids):
 
 
 @app.cell
-def _(ground_truth, ids, plt, sigma_diag, torch, y, yhat):
-    n_td = yhat.size(dim=1)
+def _(ground_truth, ids, n_td, plt, sigma_diag, torch, y, yhat):
     t = torch.arange(n_td)
 
     plt.fill_between(
         t,
-        y1=(yhat[0] - sigma_diag[0].sqrt()).real.cpu(),
-        y2=(yhat[0] + sigma_diag[0].sqrt()).real.cpu(),
+        y1=(yhat - sigma_diag.sqrt()).real.cpu(),
+        y2=(yhat + sigma_diag.sqrt()).real.cpu(),
         alpha=0.1,
         color="grey",
     )
     plt.plot(t, ground_truth(t.cuda()).real[0].cpu(), alpha=0.2)
-    plt.plot(t, yhat[0].real.cpu(), c="red", alpha=0.2)
-    plt.scatter(ids.cpu(), y.real[0].cpu(), c="orange", s=10)
+    plt.plot(t, yhat.real.cpu(), c="red", alpha=0.2)
+    plt.scatter(ids.cpu(), y.real.cpu(), c="orange", s=10)
     return
 
 
 @app.cell
-def _(ground_truth, mu, plt, sigma, torch):
-    n_fd = mu.size(dim=1)
+def _(ground_truth, n_fd, sigma, torch):
     t_fd = torch.arange(n_fd).cuda()
+    f = torch.linspace(-0.5, 0.5, n_fd)
 
     spect = torch.fft.fft(
-        ground_truth(t_fd, noise=sigma)[0],
+        ground_truth(t_fd, noise=sigma).squeeze(dim=0),
         norm="ortho",
     ).roll(n_fd // 2)
+    return f, spect
 
-    plt.plot(mu[0].real.cpu())
-    plt.plot(spect.real.cpu(), alpha=0.5)
+
+@app.cell
+def _(y, yhat):
+    y.numel(), yhat.numel(), y.numel() / yhat.numel()
+    return
+
+
+@app.cell
+def _(Path, f, mu, mu_unif, plt, spect):
+    dx = 0.017
+    dy = 0.7
+
+    (_, ax) = plt.subplots(figsize=(6, 4))
+
+    ax.plot(
+        f + 2 * dx,
+        mu_unif.real.cpu() + 2 * dy,
+        color=(0.2,) * 3,
+    )
+
+    ax.plot(
+        f + 1 * dx,
+        mu.real.cpu() + 1 * dy,
+        color=(0.4,) * 3,
+    )
+
+    ax.plot(
+        f, spect.real.cpu(),
+        color=(0.7,) * 3,
+    )
+
+    ax.set_ylabel("Amplitude / a.u.")
+    ax.set_xlabel("Normalized frequency / a.u.")
+    ax.set_xlim((-0.5, 0.5))
+    ax.grid(color=(0.9,) * 3)
+
+    plt.savefig(
+        Path.cwd() / "figure-3.pdf",
+        format="pdf",
+        dpi=600,
+        pad_inches=0,
+        bbox_inches="tight",
+    )
+
+    ax
     return
 
 
