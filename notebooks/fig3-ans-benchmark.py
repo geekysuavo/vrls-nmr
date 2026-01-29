92e3277d81bd1460069b660cea100ac73aeeba2b
Bradley Worley <geekysuavo@gmail.com>
2026-01-29 02:35:44 +0000

    Rewrite final figure notebook

--- notebooks/fig3-ans-benchmark.py
+++ notebooks/fig3-ans-benchmark.py
@@ -9,9 +9,10 @@ def _():
     from pathlib import Path
 
     import matplotlib.pyplot as plt
+    import numpy as np
     import pandas as pd
     import seaborn as sns
-    return Path, pd, plt, sns
+    return Path, np, pd, plt, sns
 
 
 @app.cell
@@ -21,36 +22,92 @@ def _(pd):
 
 
 @app.cell
-def _(df, sns):
-    # example of the kind of manips we'll need to do.
-    _df = df.set_index(["identifier", "replicate", "region", "schedule"])
-    _vrls = _df.query("algorithm == 'vrls'").error
-    _ists = _df.query("algorithm == 'ists'").error
-    _err = (_ists - _vrls) / _ists
-
-    _df["rel_error_by_schedule"] = _err
-    _df = _df.reset_index().query("algorithm == 'vrls'").drop(columns=["algorithm"])
-
-    sns.histplot(x="error", hue="schedule", data=_df.query("region == 'noise'"))
-    return
-
-
-@app.cell
 def _(df):
-    agg = df.groupby(["sigma", "min_sparsity"])[["mse_noise_ans", "mse_noise_unif", "mse_noise_diff"]].mean()
-    agg
+    df
     return
 
 
 @app.cell
-def _(Path, df, plt, sns):
+def _(np, pd):
+    def relative_error_by_algorithm(df: pd.DataFrame) -> pd.DataFrame:
+        dfs = dict(iter(df.groupby("schedule", sort=False, group_keys=False)))
+        errors = {
+            group: group_df.error.reset_index(drop=True)
+            for group, group_df in dfs.items()
+        }
+        error_unif = (errors["unif"] - errors["ans"]) / errors["unif"]
+        error_exp = (errors["exp"] - errors["ans"]) / errors["exp"]
+        error_pg = (errors["pg"] - errors["ans"]) / errors["pg"]
+        error_ans = error_pg * np.nan
+
+        error_ans.index = dfs["ans"].index
+        error_unif.index = dfs["unif"].index
+        error_exp.index = dfs["exp"].index
+        error_pg.index = dfs["pg"].index
+
+        new_df = df.copy(deep=False)
+        error = pd.concat((error_ans, error_unif, error_exp, error_pg))
+        new_df["rel_error_by_algorithm"] = error
+        return new_df
+    return (relative_error_by_algorithm,)
+
+
+@app.cell
+def _(np, pd):
+    def relative_error_by_schedule(df: pd.DataFrame) -> pd.DataFrame:
+        dfs = dict(iter(df.groupby("algorithm", sort=False, group_keys=False)))
+        errors = {
+            group: group_df.error.reset_index(drop=True)
+            for group, group_df in dfs.items()
+        }
+        error_ists = (errors["ists"] - errors["vrls"]) / errors["ists"]
+        error_fmf = (errors["fmf"] - errors["vrls"]) / errors["fmf"]
+        error_vrls = error_fmf * np.nan
+
+        error_vrls.index = dfs["vrls"].index
+        error_ists.index = dfs["ists"].index
+        error_fmf.index = dfs["fmf"].index
+
+        new_df = df.copy(deep=False)
+        error = pd.concat((error_vrls, error_ists, error_fmf))
+        new_df["rel_error_by_schedule"] = error
+        return new_df
+    return (relative_error_by_schedule,)
+
+
+@app.cell
+def _(df, relative_error_by_algorithm, relative_error_by_schedule):
+    _df = df
+
+    _columns = ["sigma", "num_components", "amplitude_dof", "min_sparsity", "algorithm"]
+    _df = _df.groupby(_columns, sort=False).apply(relative_error_by_algorithm).reset_index()
+
+    _columns = ["sigma", "num_components", "amplitude_dof", "min_sparsity", "schedule"]
+    _df = _df.groupby(_columns, sort=False).apply(relative_error_by_schedule).reset_index()
+
+    full_df = _df
+    return (full_df,)
+
+
+@app.cell
+def _(full_df):
+    full_df
+    return
+
+
+@app.cell
+def _(Path, full_df, plt, sns):
     (fig, ax) = plt.subplots(figsize=(6, 4))
 
     sns.barplot(
         x="sigma",
-        y="mse_noise_diff",
+        y="rel_error_by_algorithm",
         hue="min_sparsity",
-        data=df,
+        data=full_df.query(
+            "region == 'noise'"
+            " and schedule == 'unif'"
+            " and algorithm == 'vrls'"
+        ),
         ax=ax,
         palette=[
             (0.8,) * 3,
@@ -81,23 +138,290 @@ def _(Path, df, plt, sns):
     )
 
     fig
+    return (figure_path,)
+
+
+@app.cell
+def _(figure_path, full_df, plt, sns):
+    (_fig, (_left, _right)) = plt.subplots(nrows=1, ncols=2, figsize=(12, 4))
+
+    sns.histplot(
+        x="rel_error_by_schedule",
+        hue="schedule",
+        element="step",
+        data=full_df.query(
+            "region == 'noise'"
+            " and algorithm == 'ists'"
+        ),
+        ax=_left,
+    )
+
+    sns.histplot(
+        x="rel_error_by_schedule",
+        hue="schedule",
+        element="step",
+        data=full_df.query(
+            "region == 'signal'"
+            " and algorithm == 'ists'"
+        ),
+        ax=_right,
+        legend=False,
+    )
+
+    _left.set_title("VRLS vs. IST-S / Noise region only")
+    _left.set_xlabel("Relative error reduction")
+    _left.grid(color=(0.9,) * 3)
+    _left.set_axisbelow(True)
+
+    _right.set_title("VRLS vs. IST-S / Signal region only")
+    _right.set_xlabel("Relative error reduction")
+    _right.grid(color=(0.9,) * 3)
+    _right.set_axisbelow(True)
+    _right.set_xlim((-0.8, 0.8))
+
+    _legend = _left.get_legend()
+    _legend.set_title("Schedule")
+    _legend.texts[0].set_text("ANS")
+    _legend.texts[1].set_text("Uniform")
+    _legend.texts[2].set_text("Exponential")
+    _legend.texts[3].set_text("Poisson-gap")
+
+    plt.savefig(
+        figure_path.parent / "figure-s1.pdf",
+        format="pdf",
+        dpi=600,
+        pad_inches=0,
+        bbox_inches="tight",
+    )
+
+    _fig
     return
 
 
 @app.cell
-def _(mask, mu, mu_unif, plt, x0):
-    (_, _ax) = plt.subplots()
+def _(figure_path, full_df, plt, sns):
+    (_fig, (_left, _right)) = plt.subplots(nrows=1, ncols=2, figsize=(12, 4))
 
-    _ax.plot(x0.real.cpu())
-    _ax.plot(mu.real.cpu())
-    _ax.plot(mu_unif.real.cpu())
-    _ax.plot(mask.cpu())
-    _ax.grid(alpha=0.2)
+    sns.histplot(
+        x="rel_error_by_schedule",
+        hue="schedule",
+        element="step",
+        data=full_df.query(
+            "region == 'noise'"
+            " and algorithm == 'fmf'"
+        ),
+        ax=_left,
+    )
 
-    _ax.set_xlim((900, 1024))
-    #_ax.set_ylim((-1, 1))
+    sns.histplot(
+        x="rel_error_by_schedule",
+        hue="schedule",
+        element="step",
+        data=full_df.query(
+            "region == 'signal'"
+            " and algorithm == 'fmf'"
+        ),
+        ax=_right,
+        legend=False,
+    )
 
-    _ax
+    _left.set_title("VRLS vs. FMF-VRLS / Noise region only")
+    _left.set_xlabel("Relative error reduction")
+    _left.grid(color=(0.9,) * 3)
+    _left.set_axisbelow(True)
+
+    _right.set_title("VRLS vs. FMF-VRLS / Signal region only")
+    _right.set_xlabel("Relative error reduction")
+    _right.grid(color=(0.9,) * 3)
+    _right.set_axisbelow(True)
+    _right.set_xlim((-1, 1))
+
+    _legend = _left.get_legend()
+    _legend.set_title("Schedule")
+    _legend.texts[0].set_text("ANS")
+    _legend.texts[1].set_text("Uniform")
+    _legend.texts[2].set_text("Exponential")
+    _legend.texts[3].set_text("Poisson-gap")
+
+    plt.savefig(
+        figure_path.parent / "figure-s2.pdf",
+        format="pdf",
+        dpi=600,
+        pad_inches=0,
+        bbox_inches="tight",
+    )
+
+    _fig
+    return
+
+
+@app.cell
+def _(figure_path, full_df, plt, sns):
+    (_fig, (_left, _right)) = plt.subplots(nrows=1, ncols=2, figsize=(12, 4))
+
+    sns.histplot(
+        x="rel_error_by_algorithm",
+        hue="algorithm",
+        element="step",
+        data=full_df.query(
+            "region == 'noise'"
+            " and schedule == 'unif'"
+        ),
+        ax=_left,
+    )
+
+    sns.histplot(
+        x="rel_error_by_algorithm",
+        hue="algorithm",
+        element="step",
+        data=full_df.query(
+            "region == 'signal'"
+            " and schedule == 'unif'"
+        ),
+        ax=_right,
+        legend=False,
+    )
+
+    _left.set_title("ANS vs. Uniform / Noise region only")
+    _left.set_xlabel("Relative error reduction")
+    _left.grid(color=(0.9,) * 3)
+    _left.set_axisbelow(True)
+
+    _right.set_title("ANS vs. Uniform / Signal region only")
+    _right.set_xlabel("Relative error reduction")
+    _right.grid(color=(0.9,) * 3)
+    _right.set_axisbelow(True)
+    #_right.set_xlim((-1, 1))
+
+    _legend = _left.get_legend()
+    _legend.set_title("Algorithm")
+    _legend.texts[0].set_text("VRLS")
+    _legend.texts[1].set_text("FMF-VRLS")
+    _legend.texts[2].set_text("IST-S")
+
+    plt.savefig(
+        figure_path.parent / "figure-s3.pdf",
+        format="pdf",
+        dpi=600,
+        pad_inches=0,
+        bbox_inches="tight",
+    )
+
+    _fig
+    return
+
+
+@app.cell
+def _(figure_path, full_df, plt, sns):
+    (_fig, (_left, _right)) = plt.subplots(nrows=1, ncols=2, figsize=(12, 4))
+
+    sns.histplot(
+        x="rel_error_by_algorithm",
+        hue="algorithm",
+        element="step",
+        data=full_df.query(
+            "region == 'noise'"
+            " and schedule == 'exp'"
+        ),
+        ax=_left,
+    )
+
+    sns.histplot(
+        x="rel_error_by_algorithm",
+        hue="algorithm",
+        element="step",
+        data=full_df.query(
+            "region == 'signal'"
+            " and schedule == 'exp'"
+        ),
+        ax=_right,
+        legend=False,
+    )
+
+    _left.set_title("ANS vs. Exponential / Noise region only")
+    _left.set_xlabel("Relative error reduction")
+    _left.grid(color=(0.9,) * 3)
+    _left.set_axisbelow(True)
+    _left.set_xlim((-15, 1))
+
+    _right.set_title("ANS vs. Exponential / Signal region only")
+    _right.set_xlabel("Relative error reduction")
+    _right.grid(color=(0.9,) * 3)
+    _right.set_axisbelow(True)
+    #_right.set_xlim((-1, 1))
+
+    _legend = _left.get_legend()
+    _legend.set_title("Algorithm")
+    _legend.texts[0].set_text("VRLS")
+    _legend.texts[1].set_text("FMF-VRLS")
+    _legend.texts[2].set_text("IST-S")
+
+    plt.savefig(
+        figure_path.parent / "figure-s4.pdf",
+        format="pdf",
+        dpi=600,
+        pad_inches=0,
+        bbox_inches="tight",
+    )
+
+    _fig
+    return
+
+
+@app.cell
+def _(figure_path, full_df, plt, sns):
+    (_fig, (_left, _right)) = plt.subplots(nrows=1, ncols=2, figsize=(12, 4))
+
+    sns.histplot(
+        x="rel_error_by_algorithm",
+        hue="algorithm",
+        element="step",
+        data=full_df.query(
+            "region == 'noise'"
+            " and schedule == 'pg'"
+        ),
+        ax=_left,
+    )
+
+    sns.histplot(
+        x="rel_error_by_algorithm",
+        hue="algorithm",
+        element="step",
+        data=full_df.query(
+            "region == 'signal'"
+            " and schedule == 'pg'"
+        ),
+        ax=_right,
+        legend=False,
+    )
+
+    _left.set_title("ANS vs. Poisson-gap / Noise region only")
+    _left.set_xlabel("Relative error reduction")
+    _left.grid(color=(0.9,) * 3)
+    _left.set_axisbelow(True)
+    _left.set_xlim((-20, 1))
+
+    _right.set_title("ANS vs. Poisson-gap / Signal region only")
+    _right.set_xlabel("Relative error reduction")
+    _right.grid(color=(0.9,) * 3)
+    _right.set_axisbelow(True)
+    _right.set_xlim((-2, 0.5))
+
+    _legend = _left.get_legend()
+    _legend.set_title("Algorithm")
+    _legend.texts[0].set_text("VRLS")
+    _legend.texts[1].set_text("FMF-VRLS")
+    _legend.texts[2].set_text("IST-S")
+
+    plt.savefig(
+        figure_path.parent / "figure-s5.pdf",
+        format="pdf",
+        dpi=600,
+        pad_inches=0,
+        bbox_inches="tight",
+    )
+
+    _fig
     return
 
 
